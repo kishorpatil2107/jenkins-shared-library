3e98bdf3c9632ee3b78d922c5c9f911075ec83a8
Ubuntu <ubuntu@ip-172-31-27-106.eu-west-1.compute.internal>
2026-03-01 06:54:20 +0000

    mavenPipeline add

---
+++ vars/mavenPipeline.groovy
@@ -0,0 +1,43 @@
+def call(Map config) {
+
+    pipeline {
+        agent any
+
+        stages {
+
+            stage('Checkout') {
+                steps {
+                    git branch: config.branch,
+                        url: config.gitUrl
+                }
+            }
+
+            stage('Build') {
+                steps {
+                    sh 'mvn clean package'
+                }
+            }
+
+            stage('Test') {
+                steps {
+                    sh 'mvn test'
+                }
+            }
+
+            stage('Archive') {
+                steps {
+                    archiveArtifacts artifacts: 'target/*.jar', fingerprint: true
+                }
+            }
+        }
+
+        post {
+            success {
+                echo "Build Successful"
+            }
+            failure {
+                echo "Build Failed"
+            }
+        }
+    }
+}
