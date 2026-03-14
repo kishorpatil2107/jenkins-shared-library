f96d1d53195dcd1dcddd45bf4741702dee64c33f
Kishor Patil <149505273+kishorpatil2107@users.noreply.github.com>
2026-03-14 13:54:54 +0530

    Update Git repository URL in mavenPipeline.groovy

--- vars/mavenPipeline.groovy
+++ vars/mavenPipeline.groovy
@@ -19,7 +19,7 @@ def call(Map config) {
             stage('Checkout') {
                 steps {
                     git branch: config.branch,
-                        url: 'https://github.com/kishorpatil2107/hello-app.git',
+                        url: 'https://github.com/kishorpatil2107/jenkins-shared-library.git',
                         credentialsId: 'github-2026'
                 }
             }
